5d83717ea149aef8b2dba6201182bf888e371d41
fcoprata <fcopratan@gmail.com>
2024-03-25 17:24:44 -0300

    update build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.meuprimeiroaplicativo"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.meuprimeiroaplicativo"
         minSdk = 24
@@ -33,7 +37,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
